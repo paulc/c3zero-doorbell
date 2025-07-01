a399898cc2f7a283b7a7ee19cf3c1f2f3d54b0f1
paulc <212168+paulc@users.noreply.github.com>
2025-07-01 17:01:38 +0100

    Add SSE example

--- examples/wifi.rs
+++ examples/wifi.rs
@@ -18,12 +18,17 @@ use doorbell::ws2812::{colour, RgbLayout, Ws2812RmtSingle};
 const AP_SSID: &str = "ESP32C3-AP";
 const AP_PASSWORD: &str = "password";
 
+// Static NavBar
 pub const NAVBAR: NavBar = NavBar {
     title: "MQTT Alarm",
     links: &[
         NavLink {
             url: "/wifi",
             label: "Wifi Configuration",
+        },
+        NavLink {
+            url: "/sse_page",
+            label: "SSE Test",
         },
         NavLink {
             url: "/reset_page",
@@ -82,8 +87,65 @@ fn main() -> anyhow::Result<()> {
         home_page::make_handler(&wifi_state, NAVBAR),
     )?;
 
+    web.add_handler("/sse", Method::Get, sse::make_sse_handler())?;
+    web.add_handler("/sse_page", Method::Get, sse::make_sse_page(NAVBAR))?;
+
     loop {
         thread::sleep(Duration::from_millis(500));
+    }
+}
+
+mod sse {
+    use esp_idf_svc::http::server::{EspHttpConnection, Request};
+
+    use askama::Template;
+
+    use doorbell::web::NavBar;
+
+    #[derive(Template)]
+    #[template(path = "sse_test.html")]
+    struct SSEPage {
+        navbar: NavBar<'static>,
+    }
+
+    pub fn make_sse_page(
+        navbar: NavBar<'static>,
+    ) -> impl for<'r> Fn(Request<&mut EspHttpConnection<'r>>) -> anyhow::Result<()> + Send + 'static
+    {
+        move |request| {
+            let sse_page = SSEPage {
+                navbar: navbar.clone(),
+            };
+            let mut response = request.into_response(200, Some("OK"), &[])?;
+            let html = sse_page.render()?;
+            response.write(html.as_bytes())?;
+
+            Ok::<(), anyhow::Error>(())
+        }
+    }
+
+    pub fn make_sse_handler(
+    ) -> impl for<'r> Fn(Request<&mut EspHttpConnection<'r>>) -> anyhow::Result<()> + Send + 'static
+    {
+        move |request| {
+            let mut response = request.into_response(
+                200,
+                Some("OK"),
+                &[
+                    ("Content-Type", "text/event-stream"),
+                    ("Connection", "keep-alive"),
+                    ("Access-Control-Allow-Origin", "*"),
+                ],
+            )?;
+            for counter in 0..100 {
+                log::info!(">> Event: count={counter}");
+                let msg = format!("event: data\r\ndata: {{ \"counter\": {counter} }}\r\n\r\n");
+                response.write(msg.as_bytes())?;
+                response.flush()?;
+                std::thread::sleep(std::time::Duration::from_millis(50));
+            }
+            Ok::<(), anyhow::Error>(())
+        }
     }
 }
 
