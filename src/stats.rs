09a78c01775c5e1ac3003e585719c564fb5fa52e
paulc <212168+paulc@users.noreply.github.com>
2025-07-10 18:07:51 +0100

    Add TRIGGER const

--- src/stats.rs
+++ src/stats.rs
@@ -2,6 +2,8 @@ use crate::adc::ADC_BUFFER_LEN;
 use crate::adc::ADC_MIN_THRESHOLD;
 use crate::adc::THRESHOLD_BUFFER;
 
+const TRIGGER: f64 = 2.5;
+
 pub fn stats(buf: &[f64; ADC_BUFFER_LEN]) -> (f64, f64) {
     let mean = buf.iter().sum::<f64>() / ADC_BUFFER_LEN as f64;
     let var = buf
@@ -17,8 +19,7 @@ pub fn stats(buf: &[f64; ADC_BUFFER_LEN]) -> (f64, f64) {
 
 pub fn check_ring(mean: f64, stddev: f64, prev: &mut [f64; THRESHOLD_BUFFER]) -> (bool, f64) {
     let threshold = prev.iter().sum::<f64>() / prev.len() as f64;
-    // Trigger if stddev > 5 * threshold
-    let ring = stddev > threshold * 5_f64;
+    let ring = stddev > threshold * TRIGGER;
     if mean > ADC_MIN_THRESHOLD && !ring {
         // Update threshold buffer if above ADC_MIN_THRESHOLD and ring not deteced
         for i in 0..(THRESHOLD_BUFFER - 1) {
